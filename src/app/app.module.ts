5c46df651655c5cc62c7771e6486745ebfe649cd
dravenxiaokai <jspankai@vip.qq.com>
2018-03-18 19:02:37 +0800

    登录页面的布局开

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { DiscoveryPage } from '../pages/discovery/discovery';
 import { ChatPage } from '../pages/chat/chat';
 import { NotificationPage } from '../pages/notification/notification';
 import { MorePage } from '../pages/more/more';
+import { LoginPage } from '../pages/login/login';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -22,6 +23,7 @@ import { RestProvider } from '../providers/rest/rest';
     ChatPage,
     NotificationPage,
     MorePage,
+    LoginPage,
     TabsPage
   ],
   imports: [
@@ -31,11 +33,12 @@ import { RestProvider } from '../providers/rest/rest';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    HomePage,
     DiscoveryPage,
     ChatPage,
     NotificationPage,
     MorePage,
-    HomePage,
+    LoginPage,
     TabsPage
   ],
   providers: [
--- src/pages/more/more.ts
+++ src/pages/more/more.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ModalController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
 
 /**
  * Generated class for the MorePage page.
@@ -8,18 +9,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
 @Component({
   selector: 'page-more',
   templateUrl: 'more.html',
 })
 export class MorePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public modalCtrl: ModalController) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MorePage');
+  showModal() {
+    let modal = this.modalCtrl.create(LoginPage);
+    modal.present();
   }
 
 }
