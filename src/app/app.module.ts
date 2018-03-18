912199981190b5f868882ea95f42e9a771bb5058
dravenxiaokai <jspankai@vip.qq.com>
2018-03-18 16:12:46 +0800

    五个一级基础页面建立

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { DiscoveryPage } from '../pages/discovery/discovery';
+import { ChatPage } from '../pages/chat/chat';
+import { NotificationPage } from '../pages/notification/notification';
+import { MorePage } from '../pages/more/more';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,9 +16,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    DiscoveryPage,
+    ChatPage,
+    NotificationPage,
+    MorePage,
     TabsPage
   ],
   imports: [
@@ -26,8 +30,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    DiscoveryPage,
+    ChatPage,
+    NotificationPage,
+    MorePage,
     HomePage,
     TabsPage
   ],
